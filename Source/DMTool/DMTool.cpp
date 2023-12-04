418cef639b2dedd754af00234dd5d2efd9701f48
GKeC11 <695266749@qq.com>
2023-12-04 20:02:42 +0800

    Init

---
+++ Source/DMTool/DMTool.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DMTool.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DMTool, "DMTool" );
+
+DEFINE_LOG_CATEGORY(LogDMTool)
+ 
---
+++ Source/DMTool/DMToolGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DMToolGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADMToolGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADMToolGameMode();
+};
+
+
+
